25f7ee65ccf7fc0c6726b1916fee45462a85ac70
Elizabeth Bassey <40176275+Bassey-Elizabeth@users.noreply.github.com>
2020-04-08 22:50:18 +0100

    JavaScript task 2

---
+++ js_tasks/task_2.js
@@ -0,0 +1,60 @@
+ /*****************
+ JAVASCRIPT TASK2
+ *****************/
+
+ //array of objects
+ const data = [
+     {
+         principal: 2500,
+         time: 1.8
+     },
+     {
+         principal: 1000,
+         time: 5
+     },
+     {
+         principal: 3000,
+         time: 1
+     },
+     {
+         principal: 2000,
+         time: 3
+     }
+ ];
+
+//function interestCalculator
+ const interestCalculator = (arr) => {
+ 	 let rate;
+     arr.forEach(item => {
+         if ( item.principal >= 2500 && (item.time > 1 && item.time < 3 ) ) {
+             return rate = 3;
+         } else if (item.principal >= 2500 && item.time >= 3 ) {
+             return rate = 4;
+         } else if (item.principal < 2500 || item.time <= 1 ) {
+             return rate = 2
+         } else {
+             return rate = 1;
+         }
+     
+     let interest = (principal * rate * time) / 100;
+    
+     let interestData = arr.map (() => {
+                    return {
+     					principal: item.principal,
+     					rate: rate,
+     					time: item.time,
+     					interest: interest
+     				}
+     			});
+
+     console.log(interestData);
+
+ 	  return interestData;
+ });
+
+ };
+
+//call function interestCalculator
+ interestCalculator(data);
+
+
